36740b6184fb816f1e02dabb831ce6698a898962
cw9 <chaow929@gmail.com>
2021-06-16 12:13:11 -0400

    pr

--- src/cmd/scrapevalidator/main.go
+++ src/cmd/scrapevalidator/main.go
@@ -10,7 +10,7 @@ import (
 )
 
 var (
-	endpointArg       = flag.String("endpoint", "", "prom endpoint to validate")
+	endpointArg       = flag.String("endpoint", "", "prom endpoint to validate, this is required")
 	scrapeTimeoutArg  = flag.Duration("scrape-timeout", 8*time.Second, "timeout for each scrape")
 	scrapeIntervalArg = flag.Duration("scrape-interval", 10*time.Second, "time between scrapes")
 	errorLevelArg     = flag.String("error-level", "must", "OpenMetrics defines rules in different categories like \"SHOULD\" and \"MUST\", by default this parameter is set to \"must\" so that only the rules in the \"MUST\" category is checked, the alternative value is \"should\" which validates the rules in both categories.")
